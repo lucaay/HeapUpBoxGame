094342a1d946e56fdfb887a87a5e11b2f9766d33
Luca Sergiu <you@example.com>
2023-01-10 00:06:06 +0200

    big structure setup

--- src/main/java/com/sergiu/heapupboxgame/SceneController.java
+++ src/main/java/com/sergiu/heapupboxgame/SceneController.java
@@ -1,10 +1,12 @@
 package com.sergiu.heapupboxgame;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,6 +26,17 @@ public class SceneController {
     }
     public void switchToHomeScreen(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("home-screen.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToLevel(ActionEvent event) throws IOException {
+        Object node = event.getSource(); //returns the object that generated the event
+        //since the returned object is a Button you can cast it to one
+        Button b = (Button)node;
+        int level = Integer.parseInt(b.getText());
+        root = FXMLLoader.load(getClass().getResource("levels/level-" + level + ".fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
