cdc24f89bb8232d11ce8ec0070079c05cde978fa
Luca Sergiu <you@example.com>
2023-01-09 21:44:22 +0200

    basic game window

---
+++ src/main/java/com/sergiu/heapupboxgame/SceneController.java
@@ -0,0 +1,32 @@
+package com.sergiu.heapupboxgame;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToLevelSelector(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("level-selector-screen.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToHomeScreen(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("home-screen.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
